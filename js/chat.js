3ba4e2e50c513ce405fb0df644f920d7b46a146f
Elizabeth Hudnott <lizzy.hudnott@gmail.com>
2018-03-30 17:58:21 +0100

    Update to allow server customization.

--- js/chat.js
+++ js/chat.js
@@ -1,6 +1,4 @@
 "use strict";
-const peerAPIKey = 'spknmd8wnib2o6r';
-
 var sessionBadge = $('#session-badge');
 var alertArea = $('#alerts');
 var connectButton = $('#connect-btn');
@@ -20,19 +18,7 @@ const slideShareURL = /http(s)?:\/\/www.slideshare.net\/([\w-]+\/[\w-]+)((\/?$)|
 
 var joinRequests = [];
 var connected = false;
-var p2p = new P2P(
-	function (error) {
-		chatWindow.append(`
-			<div class="chat system-message">
-				Error: ${error.message}
-			</div>
-		`);
-		console.error(error);
-		debugger;
-	},
-	{key: peerAPIKey, debug: 2}
-);
-var myUserID;
+var p2p, myUserID;
 
 function processJoinRequest() {
 	if (modalDisplayed) {
@@ -86,6 +72,7 @@ connectButton.on('click', function (event) {
 	} else {
 		if (document.getElementById('login-form').checkValidity()) {
 			event.preventDefault();
+			initializeNetworking();
 			connected = true;
 			$('.login-detail').slideUp();
 			connectButton.html('Disconnect');
@@ -126,118 +113,145 @@ $('#confirm-action-btn').on('click', function (event) {
 	confirmationModal.modal('hide');
 });
 
-p2p.on('connected', function (event) {
-	alertArea.append(`
-		<div class="alert alert-info" id="pending-join-alert">
-			Connected to ${event.sessionID}. Waiting for permission to join the conversation&hellip;
-		</div>
-	`);
-});
-
-p2p.on('joined', function (event) {
-	messageBox[0].focus();
-
-	$('#pending-join-alert').remove();
-	var alert = $(`
-		<div class="alert alert-success alert-dismissible">
-			You're now part of the conversation "${event.sessionID}".
-			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-				<span aria-hidden="true">&times;</span>
-			</button>
-  		</div>
-	`);
-	alertArea.append(alert);
-	fadeOutAndRemove(alert);
-
-	chatWindow.append(`
-		<div class="chat system-message">
-			<span class="user-id">${myUserID}</span> is present.
-		</div>
-	`);
-
-	sessionBadge.html('<span class="sr-only">The current session name is </span>' + event.sessionID);
-	sessionBadge.css('visibility', 'visible');
-
-	if (event.isAdmin) {
-		$('#ban-user-btn').show();
+function initializeNetworking() {
+	//Format: API-key@hostname/path:port, preceded by wss:// for an encrypted connection.
+	let url = $('#server-url').val().match(/^(ws(?:s)?:\/\/)?(?:([^@]*)@)?([^/:]+)(\/[^:]*)?(?::(\d+))?$/);
+	let connectionOptions = {
+		secure: url[1] === 'wss://',
+		host: url[3],
+		path: url[4] || '/',
+		port: url[5] || 80,
+		debug: 2
+	};
+	if (url[2] !== undefined) {
+		connectionOptions.key = url[2];
 	}
-});
+	p2p = new P2P(
+		function (error) {
+			chatWindow.append(`
+				<div class="chat system-message">
+					Error: ${error.message}
+				</div>
+			`);
+			console.error(error);
+			debugger;
+		},
+		connectionOptions
+	);
 
-p2p.on('ejected', function (event) {
-	$('#pending-join-alert').remove();
-	alertArea.append(`
-		<div class="alert alert-warning">
-			${event.message}
-		</div>
-	`);
-	disconnected();
-});
+	p2p.on('connected', function (event) {
+		alertArea.append(`
+			<div class="alert alert-info" id="pending-join-alert">
+				Connected to ${event.sessionID}. Waiting for permission to join the conversation&hellip;
+			</div>
+		`);
+	});
 
-p2p.on('joinrequest', function (event) {
-	if (event.userID === 'everyone') {
-		p2p.rejectUser(userID);
-	} else {
-		joinRequests.push(event.userID);
-		if (joinRequests.length == 1) {
-			processJoinRequest();
+	p2p.on('joined', function (event) {
+		messageBox[0].focus();
+
+		$('#pending-join-alert').remove();
+		var alert = $(`
+			<div class="alert alert-success alert-dismissible">
+				You're now part of the conversation "${event.sessionID}".
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+	  		</div>
+		`);
+		alertArea.append(alert);
+		fadeOutAndRemove(alert);
+
+		chatWindow.append(`
+			<div class="chat system-message">
+				<span class="user-id">${myUserID}</span> is present.
+			</div>
+		`);
+
+		sessionBadge.html('<span class="sr-only">The current session name is </span>' + event.sessionID);
+		sessionBadge.css('visibility', 'visible');
+
+		if (event.isAdmin) {
+			$('#ban-user-btn').show();
 		}
-	}
-});
+	});
 
-p2p.on('userpresent', function (event) {
-	var userID = event.userID;
-	chatWindow.append(`
-		<div class="chat system-message">
-			<span class="user-id">${userID}</span> is present.
-		</div>
-	`);
-	var userListOptions = userList.children();
-	var newOption = `<option value="${userID}">${userID}</option>`;
-	var inserted = false;
-	for (let i = 0; i < userListOptions.length; i++) {
-		let option = userListOptions.eq(i);
-		let value = option.attr('value');
-		if (value > userID && value !== 'everyone') {
-			$(newOption).insertBefore(option);
-			inserted = true;
-			break;
+	p2p.on('ejected', function (event) {
+		$('#pending-join-alert').remove();
+		alertArea.append(`
+			<div class="alert alert-warning">
+				${event.message}
+			</div>
+		`);
+		disconnected();
+	});
+
+	p2p.on('joinrequest', function (event) {
+		if (event.userID === 'everyone') {
+			p2p.rejectUser(userID);
+		} else {
+			joinRequests.push(event.userID);
+			if (joinRequests.length == 1) {
+				processJoinRequest();
+			}
 		}
-	}
-	if (!inserted) {
-		userList.append(newOption);
-	}
-});
+	});
 
-p2p.on('userleft', function (event) { 
-	var userID = event.userID
-	chatWindow.append(`
-		<div class="chat system-message">
-			<span class="user-id">${userID}</span>
-			has left the conversation.
-		</div>
-	`);
-	userList.children(`[value=${userID}]`).remove();
-});
+	p2p.on('userpresent', function (event) {
+		var userID = event.userID;
+		chatWindow.append(`
+			<div class="chat system-message">
+				<span class="user-id">${userID}</span> is present.
+			</div>
+		`);
+		var userListOptions = userList.children();
+		var newOption = `<option value="${userID}">${userID}</option>`;
+		var inserted = false;
+		for (let i = 0; i < userListOptions.length; i++) {
+			let option = userListOptions.eq(i);
+			let value = option.attr('value');
+			if (value > userID && value !== 'everyone') {
+				$(newOption).insertBefore(option);
+				inserted = true;
+				break;
+			}
+		}
+		if (!inserted) {
+			userList.append(newOption);
+		}
+	});
 
-p2p.on('message', function (event) {
-	var text = formatAsHTML(event.message);
-	var scrolledToBottom = chatWindow.scrollTop() >= chatWindow[0].scrollHeight - chatWindow.height() - 1;
-	var annotation;
-	if (event.isPrivate) {
-		annotation = ' (Private)';
-	} else {
-		annotation = '';
-	}
-	chatWindow.append(`
-		<div class="chat">
-			<span class="user-id">${event.userID}${annotation}:</span>
-			<pre>${text}</pre>
-		</div>
-	`);
-	if (scrolledToBottom) {
-		chatWindow.scrollTop(chatWindow[0].scrollHeight);
-	}
-});
+	p2p.on('userleft', function (event) { 
+		var userID = event.userID
+		chatWindow.append(`
+			<div class="chat system-message">
+				<span class="user-id">${userID}</span>
+				has left the conversation.
+			</div>
+		`);
+		userList.children(`[value=${userID}]`).remove();
+	});
+
+	p2p.on('message', function (event) {
+		var text = formatAsHTML(event.message);
+		var scrolledToBottom = chatWindow.scrollTop() >= chatWindow[0].scrollHeight - chatWindow.height() - 1;
+		var annotation;
+		if (event.isPrivate) {
+			annotation = ' (Private)';
+		} else {
+			annotation = '';
+		}
+		chatWindow.append(`
+			<div class="chat">
+				<span class="user-id">${event.userID}${annotation}:</span>
+				<pre>${text}</pre>
+			</div>
+		`);
+		if (scrolledToBottom) {
+			chatWindow.scrollTop(chatWindow[0].scrollHeight);
+		}
+	});
+}
 
 function resizeMessageBox() {
 	messageBox.css('min-height', '');
